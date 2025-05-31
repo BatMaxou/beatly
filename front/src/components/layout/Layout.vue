a2a8280bb627f32c1052a51628e74c0acf972183
DvK42 <guillaume.couvidou@live.fr>
2025-05-31 16:28:21 +0200

    Mise en place du Layout + Routeur de navigation

---
+++ front/src/components/layout/Layout.vue
@@ -0,0 +1,26 @@
+<script setup>
+defineProps({
+  currentView: {
+    type: Object,
+    required: true
+  }
+})
+import UiNavigateButton from '@/components/buttons/UiNavigateButton.vue'
+</script>
+
+<template>
+  <div class="layout">
+    <main>
+        {{ console.log(currentView) }}
+      <component :is="currentView" />
+    </main>
+    <UiNavigateButton />
+  </div>
+</template>
+
+<style scoped>
+.layout {
+  position: relative;
+  min-height: 100vh;
+}
+</style>
