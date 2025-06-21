0508df1f6e27354aef42785859eddcec265ba28f
BatMaxou <maximebatista.lycee@gmail.com>
2025-06-21 22:37:04 +0200

    :sparkles: Add User / Artist register

---
+++ api/src/Domain/Command/RegisterHandler.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Domain\Command;
+
+use App\Entity\User;
+use App\Enum\RoleEnum;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Messenger\Attribute\AsMessageHandler;
+
+#[AsMessageHandler]
+class RegisterHandler
+{
+    public function __construct(
+        private readonly UserRepository $userRepository,
+        private readonly EntityManagerInterface $em,
+    ) {
+    }
+
+    public function __invoke(RegisterCommand $command): Response {
+        if ($this->userRepository->findOneBy(['email' => $command->email])) {
+            return new JsonResponse(['error' => 'Email already exists'], 409);
+        }
+
+        $newUser = (new User())
+            ->setEmail($command->email)
+            ->setName($command->name)
+            ->setPassword($command->password);
+
+        if ($command->registerType === RegisterCommand::ARTIST_REGISTER) {
+            $newUser->addRole(RoleEnum::ARTIST);
+        }
+
+        $this->em->persist($newUser);
+        $this->em->flush();
+
+        return new JsonResponse(['response' => 'User created'], 201);
+    }
+}
