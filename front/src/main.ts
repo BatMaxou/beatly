9b0e30adc609cba735076b763d167a5522c6fda7
BatMaxou <maximebatista.lycee@gmail.com>
2025-06-24 23:57:25 +0200

    :sparkles: Add api client

--- front/src/main.ts
+++ front/src/main.ts
@@ -2,6 +2,8 @@ import "./assets/main.css";
 import "./assets/main-build.css";
 
 import { createApp } from "vue";
+import { createPinia } from "pinia";
+
 import App from "./App.vue";
 
-createApp(App).mount("#app");
+createApp(App).use(createPinia()).mount("#app");
