d714363be4484fc90ecc5581d5c21850770fe652
DvK42 <guillaume.couvidou@live.fr>
2025-07-04 00:33:03 +0200

    ➕ ADD Formkit

--- front/src/main.ts
+++ front/src/main.ts
@@ -3,8 +3,9 @@ import "./assets/main-build.css";
 
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import { plugin, defaultConfig } from "@formkit/vue";
 import router from "./router";
 
 import App from "./App.vue";
 
-createApp(App).use(createPinia()).use(router).mount("#app");
+createApp(App).use(createPinia()).use(router).use(plugin, defaultConfig).mount("#app");
