4690701b4a869e0c801a403d25694a38820a7c24
DvK42 <guillaume.couvidou@live.fr>
2025-07-04 14:09:44 +0200

    ➕ ADD Trad FormKit

--- front/src/main.ts
+++ front/src/main.ts
@@ -3,9 +3,20 @@ import "./assets/main-build.css";
 
 import { createApp } from "vue";
 import { createPinia } from "pinia";
-import { plugin, defaultConfig } from "@formkit/vue";
+import { plugin as FormKitPlugin, defaultConfig } from "@formkit/vue";
+import { fr } from "@formkit/i18n";
 import router from "./router";
 
 import App from "./App.vue";
 
-createApp(App).use(createPinia()).use(router).use(plugin, defaultConfig).mount("#app");
+createApp(App)
+  .use(createPinia())
+  .use(router)
+  .use(
+    FormKitPlugin,
+    defaultConfig({
+      locales: { fr },
+      locale: "fr",
+    }),
+  )
+  .mount("#app");
