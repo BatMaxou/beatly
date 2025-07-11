e2c81786306261060437388048016eefd3f0fb6f
DvK42 <guillaume.couvidou@live.fr>
2025-07-11 16:27:52 +0200

    🐛 Fix affichage playlist item

--- front/src/components/lists/PlaylistMusicListItem.vue
+++ front/src/components/lists/PlaylistMusicListItem.vue
@@ -2,6 +2,8 @@
 import { defineProps, defineEmits, ref, computed } from "vue";
 import MusicPlayButton from "@/components/lists/MusicPlayButton.vue";
 import PlaylistTitleMenu from "../menus/PlaylistTitleMenu.vue";
+import defaultCover from "@/assets/images/default-cover.png";
+
 const ressourceUrl = import.meta.env.VITE_API_RESSOURCES_URL;
 const props = defineProps({
   music: {
@@ -31,7 +33,7 @@ const emit = defineEmits(["toggle-play"]);
 const musicInformations = computed(() => {
   return {
     title: props.music.music.title,
-    cover: ressourceUrl + props.music.music.cover,
+    cover: props.music.music.cover ? ressourceUrl + props.music.music.cover : defaultCover,
     position: props.music.position,
     albums: props.music.music.albums || [],
     artists: props.music.music.artists || [],
@@ -59,14 +61,13 @@ const textColor = computed(() => {
 
 <template>
   <div
-    class="flex flex-row items-center h-14 px-4 transition-colors duration-200 ease-in-out cursor-pointer border-b border-gray-200/20 hover:bg-black/80 hover:text-white"
+    class="flex flex-row justify-between items-center h-14 px-4 transition-colors duration-200 ease-in-out cursor-pointer border-b border-gray-200/20 hover:bg-black/80 hover:text-white"
     :class="{ 'bg-black/80 text-white': isPlaying }"
     @mouseenter="handleMouseEnter"
     @mouseleave="handleMouseLeave"
-    @click="handleRowClick"
   >
-    <div class="flex flex-row items-center gap-2">
-      <div class="relative flex items-center justify-center w-12">
+    <div class="flex flex-row items-center w-full gap-2">
+      <div class="relative flex items-center justify-center w-12 min-w-12">
         <img :src="musicInformations.cover" class="w-12 h-12 rounded" />
         <div
           v-if="isHovered"
@@ -79,16 +80,21 @@ const textColor = computed(() => {
           />
         </div>
       </div>
-
       <div class="flex flex-col">
         <span class="font-medium" :class="textColor">{{ musicInformations.title }}</span>
         <p class="font-medium" :class="textColor">
-          <span v-for="(artist) in musicInformations.artists" :key="artist.id" class="text-white/70 text-sm">
+          <span
+            v-for="artist in musicInformations.artists"
+            :key="artist.id"
+            class="text-white/70 text-sm"
+          >
             {{ artist.name }}
           </span>
         </p>
       </div>
-      <PlaylistTitleMenu :isFavorite="false" />
+    </div>
+    <div class="text-gray-500 font-normal">
+      <PlaylistTitleMenu :isFavorite="false" position="bottom-right" />
     </div>
   </div>
 </template>
