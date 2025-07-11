8302074fcedb726cedcc710ccda2ac3032a4dc4c
DvK42 <guillaume.couvidou@live.fr>
2025-07-11 17:08:54 +0200

    Fix homepage scrollbar & navigation

--- front/src/components/layout/InAppLayout.vue
+++ front/src/components/layout/InAppLayout.vue
@@ -4,8 +4,7 @@ import { useAuthStore } from "@/stores/auth";
 import { useToast } from "@/composables/useToast";
 import loadingIcon from "@/assets/icons/loading-light.svg";
 import PlayerSection from "@/components/player/PlayerSection.vue";
-import { usePlayerStore } from '@/stores/player'
-
+import { usePlayerStore } from "@/stores/player";
 
 // Props
 const { loading, padding } = defineProps({
@@ -19,7 +18,7 @@ const { loading, padding } = defineProps({
   },
 });
 
-const playerStore = usePlayerStore()
+const playerStore = usePlayerStore();
 const authStore = useAuthStore();
 const { showSuccess } = useToast();
 
@@ -27,34 +26,41 @@ if (authStore.loginSuccess) {
   showSuccess("Vous êtes connecté !");
   authStore.setLoginSuccess(false);
 }
-
 </script>
 
 <template>
   <div class="relative min-h-screen text-white">
     <div
       class="absolute inset-0 z-10"
-      style="background: linear-gradient(to top, #5523BF, #B00D72); opacity: 1"
+      style="background: linear-gradient(to top, #5523bf, #b00d72); opacity: 1"
     ></div>
 
-    <div :class="playerStore.isPlayerActive ? 'playingMinHeight z-20 flex' : 'min-h-screen z-20 flex'">
+    <div
+      :class="playerStore.isPlayerActive ? 'playingMinHeight z-20 flex' : 'min-h-screen z-20 flex'"
+    >
       <!-- Sidebar -->
-      <SideBar class="fixed left-0 top-0 min-w-[250px] w-full max-w-[250px] z-20"/>
-    
+      <SideBar class="fixed left-0 top-0 min-w-[250px] w-full max-w-[250px] z-20" />
+
       <!-- Emplacement du contenu de la page -->
-      <div :class="'ms-[250px] z-20 w-full relative ' + padding">
-          <div v-if="loading" class="absolute inset-0 flex flex-col items-center justify-center">
-            <img :src="loadingIcon" alt="Chargement" class="h-12 w-12 animate-spin mb-4" />
-          </div>
-          <div :class="loading ? 'opacity-0 transition ease duration-700' : 'opacity-1 transition ease duration-700'">
-            <slot v-if="!loading"></slot>
-          </div>
+      <div :class="'ms-[250px] z-20 w-full overflow-hidden relative ' + padding">
+        <div v-if="loading" class="absolute inset-0 flex flex-col items-center justify-center">
+          <img :src="loadingIcon" alt="Chargement" class="h-12 w-12 animate-spin mb-4" />
         </div>
+        <div
+          :class="
+            loading
+              ? 'opacity-0 transition ease duration-700'
+              : 'opacity-1 transition ease duration-700'
+          "
+        >
+          <slot v-if="!loading"></slot>
+        </div>
+      </div>
 
       <!-- Player en bas de page -->
     </div>
     <div class="fixed bottom-0 left-0 right-0 z-20 bg-[#2E0B40] flex justify-center items-center">
-      <PlayerSection/>
+      <PlayerSection />
     </div>
   </div>
 </template>
--- front/src/pages/HomePage.vue
+++ front/src/pages/HomePage.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { onBeforeMount, ref } from "vue";
+import { onBeforeMount, ref, onMounted } from "vue";
 import InAppLayout from "@/components/layout/InAppLayout.vue";
 import { useApiClient } from "@/stores/api-client";
 import { useUserStore } from "@/stores/user";
@@ -7,6 +7,8 @@ import PlaylistPlayable from "@/components/cards/PlaylistPlayableCard.vue";
 import AlbumPlayableCard from "@/components/cards/AlbumPlayableCard.vue";
 import albumDefaultIcon from "@/assets/icons/disc-dark.svg";
 import type { Playlist } from "@/utils/types";
+import arrowLeft from "@/assets/icons/arrow-left-light.svg";
+import arrowRight from "@/assets/icons/arrow-right-light.svg";
 
 const { apiClient } = useApiClient();
 const userStore = useUserStore();
@@ -18,47 +20,101 @@ const hitsList = ref([]);
 const categoryList = ref([]);
 const mostFavoriteList = ref([]);
 const loading = ref(false);
+const playlistSlider = ref<HTMLElement | null>(null);
+const showNextButton = ref(true);
+const showPrevButton = ref(false);
+
+function scrollPlaylistSlider() {
+  if (playlistSlider.value) {
+    const elementWidth = playlistSlider.value.firstElementChild?.clientWidth || 0;
+    const gap = 64;
+    const scrollDistance = (elementWidth + gap) * 3;
+    playlistSlider.value.scrollBy({ left: scrollDistance, behavior: "smooth" });
+  }
+}
+
+function scrollPlaylistSliderBack() {
+  if (playlistSlider.value) {
+    const elementWidth = playlistSlider.value.firstElementChild?.clientWidth || 0;
+    const gap = 64;
+    const scrollDistance = (elementWidth + gap) * 3;
+    playlistSlider.value.scrollBy({ left: -scrollDistance, behavior: "smooth" });
+  }
+}
+
+function checkScrollEnd() {
+  if (playlistSlider.value) {
+    const { scrollLeft, scrollWidth, clientWidth } = playlistSlider.value;
+    showNextButton.value = scrollLeft + clientWidth < scrollWidth - 10; // 10px de marge
+    showPrevButton.value = scrollLeft > 10;
+  }
+}
 
 onBeforeMount(async () => {
   loading.value = true;
   try {
     const response = await apiClient.playlist.getAll();
-    
+
     const playlists: Playlist[] = response;
-    
+
     lastPlaylist.value = playlists.slice(0, 10);
-    
   } catch (error) {
-    console.error('Erreur lors de la récupération des playlists:', error);
+    console.error("Erreur lors de la récupération des playlists:", error);
     lastPlaylist.value = [];
   } finally {
     loading.value = false;
   }
 });
+
+onMounted(() => {
+  setTimeout(() => {
+    checkScrollEnd();
+  }, 100);
+});
 </script>
 
 <template>
-  <InAppLayout :loading="loading">
+  <InAppLayout :loading="loading" padding="pt-10">
     <!-- Écoutes récentes si il y en a -->
-     <div v-if="recentlyListened.length > 0">
-      <h2 class="text-white text-3xl font-bold mb-4">Écoutes récentes</h2>
+    <div v-if="recentlyListened.length > 0">
+      <h2 class="ps-10 text-white text-3xl font-bold mb-4">Écoutes récentes</h2>
     </div>
 
     <div v-if="lastPlaylist.length > 0">
-      <h2 class="text-white text-3xl font-bold mb-4">Dernières playlists</h2>
-      <div class="flex flex-row justify-start gap-16 overflow-x-auto">
-        <PlaylistPlayable
-          v-for="playlist in lastPlaylist"
-          :key="playlist.id"
-          :playlistId="playlist.id"
-          :playlistCover="playlist.cover || albumDefaultIcon"
-          :playlistName="playlist.title || 'Playlist sans nom'"
-        />
+      <h2 class="ps-10 text-white text-3xl font-bold mb-4">Dernières playlists</h2>
+      <div class="relative">
+        <div
+          class="flex flex-row justify-start gap-16 overflow-x-auto scrollbar-hide ps-10"
+          ref="playlistSlider"
+          @scroll="checkScrollEnd"
+        >
+          <PlaylistPlayable
+            v-for="playlist in lastPlaylist"
+            :key="playlist.id"
+            :playlistId="playlist.id"
+            :playlistCover="playlist.cover || albumDefaultIcon"
+            :playlistName="playlist.title || 'Playlist sans nom'"
+          />
+        </div>
+        <button
+          v-if="showPrevButton"
+          class="absolute left-4 top-1/2 p-6 transform -translate-y-1/2 bg-black opacity-70 rounded-full shadow-md hover:opacity-90 transition-opacity"
+          @click="scrollPlaylistSliderBack"
+        >
+          <img :src="arrowLeft" class="w-8 h-8" alt="Flèche gauche" />
+        </button>
+        <button
+          v-if="showNextButton"
+          class="absolute right-4 top-1/2 p-6 transform -translate-y-1/2 bg-black opacity-70 rounded-full shadow-md hover:opacity-90 transition-opacity"
+          @click="scrollPlaylistSlider"
+        >
+          <img :src="arrowRight" class="w-8 h-8" alt="Flèche gauche" />
+        </button>
       </div>
     </div>
 
     <div v-if="recommendationList.length > 0">
-      <h2 class="text-white text-3xl font-bold mb-4">D'après vos écoutes...</h2>
+      <h2 class="ps-10 text-white text-3xl font-bold mb-4">D'après vos écoutes...</h2>
       <AlbumPlayableCard
         :albumCover="albumDefaultIcon"
         albumName="Mon album préféré !"
@@ -68,15 +124,18 @@ onBeforeMount(async () => {
     </div>
 
     <div v-if="hitsList.length > 0">
-      <h2 class="text-white text-3xl font-bold mb-4">Hits du moment !</h2><!-- Les + écoutés  -->
+      <h2 class="ps-10 text-white text-3xl font-bold mb-4">Hits du moment !</h2>
+      <!-- Les + écoutés  -->
     </div>
 
     <div v-if="categoryList.length > 0">
-      <h2 class="text-white text-3xl font-bold mb-4">Pour une envie particulière</h2><!-- Liste des catégories  -->
+      <h2 class="ps-10 text-white text-3xl font-bold mb-4">Pour une envie particulière</h2>
+      <!-- Liste des catégories  -->
     </div>
 
     <div v-if="mostFavoriteList.length > 0">
-      <h2 class="text-white text-3xl font-bold mb-4">Ils font l'unanimité</h2><!-- Les + mis en favoris  -->
+      <h2 class="ps-10 text-white text-3xl font-bold mb-4">Ils font l'unanimité</h2>
+      <!-- Les + mis en favoris  -->
     </div>
   </InAppLayout>
 </template>
