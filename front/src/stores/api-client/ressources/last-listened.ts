5bdee35c37f9fbca2b61c708caf4f5a3151332cb
BatMaxou <maximebatista.lycee@gmail.com>
2025-07-17 19:58:15 +0200

    :sparkles: Get collection last listened of current user

---
+++ front/src/stores/api-client/ressources/last-listened.ts
@@ -0,0 +1,18 @@
+import type { LastListened as LastListenedType, Playlist, Music, Album } from "@/utils/types";
+import type { ApiClient, CollectionResponse } from "../model";
+
+interface LastListenedResource extends LastListenedType<Music | Playlist | Album> {};
+
+const ApiRessourcePath = "/last_listened";
+
+export default class LastListened {
+  apiClient: ApiClient;
+
+  constructor(apiClient: ApiClient) {
+    this.apiClient = apiClient;
+  }
+
+  async getAll(): Promise<CollectionResponse<LastListenedResource>> {
+    return this.apiClient.get<CollectionResponse<LastListenedResource>>(ApiRessourcePath, { Accept: "application/ld+json" });
+  }
+}
